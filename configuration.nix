66d9d9dfc5c246b9f9a7f0c11ffa91327f7f29d0
MarcusBoay <marcusboay@hotmail.com>
2024-06-22 20:45:41 -0400

    run gc automatically

--- configuration.nix
+++ configuration.nix
@@ -168,7 +168,14 @@
     ];
   };
 
-  nix.settings.experimental-features = [ "nix-command flakes" ];
+  nix = {
+    settings.experimental-features = [ "nix-command flakes" ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
   nixpkgs.config.allowUnfree = true;
   system.autoUpgrade = {
     enable = true;
