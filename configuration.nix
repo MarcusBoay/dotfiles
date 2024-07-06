f29b5fad7a438f43fc27637d6137e6e8049cda72
MarcusBoay <marcusboay@hotmail.com>
2024-07-05 23:55:48 -0400

    update nvidia driver to 555.58.02

--- configuration.nix
+++ configuration.nix
@@ -74,12 +74,12 @@
     bluetooth.enable = true;
     nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "555.52.04";
-        sha256_64bit = "sha256-nVOubb7zKulXhux9AruUTVBQwccFFuYGWrU1ZiakRAI=";
-        sha256_aarch64 = lib.fakeSha256;
-        openSha256 = lib.fakeSha256;
-        settingsSha256 = "sha256-PMh5efbSEq7iqEMBr2+VGQYkBG73TGUh6FuDHZhmwHk="; 
-        persistencedSha256 = lib.fakeSha256;
+        version = "555.58.02";
+        sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
+        sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
+        openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
+        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
+        persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
       };
       modesetting.enable = true;
       forceFullCompositionPipeline = true; # fix screen tearing
