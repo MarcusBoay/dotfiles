a90752f3e2d87700ef88142d35f1f067f0d971a3
MarcusBoay <marcusboay@hotmail.com>
2024-12-07 16:39:01 -0500

    24.11 update

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,6 @@
   };
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -81,21 +80,19 @@
 
   hardware = {
     bluetooth.enable = true;
-    nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "555.58.02";
-        sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
-        sha256_aarch64 = "sha256-wb20isMrRg8PeQBU96lWJzBMkjfySAUaqt4EgZnhyF8=";
-        openSha256 = "sha256-8hyRiGB+m2hL3c9MDA/Pon+Xl6E788MZ50WrrAGUVuY=";
-        settingsSha256 = "sha256-ZpuVZybW6CFN/gz9rx+UJvQ715FZnAOYfHn5jt5Z2C8=";
-        persistencedSha256 = "sha256-a1D7ZZmcKFWfPjjH1REqPM5j/YLWKnbkP9qfRyIyxAw=";
+    bluetooth.settings = {
+      General = {
+        Experimental = true;
       };
+    };
+    nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+      open = false;
       modesetting.enable = true;
     };
-    opengl = {
+    graphics = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      enable32Bit = true;
     };
 
     keyboard.qmk.enable = true;
@@ -115,6 +112,7 @@
       kate
       vscode
       libreoffice
+      krita
     ];
     useDefaultShell = true;
   };
